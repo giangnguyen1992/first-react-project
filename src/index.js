b7cf17a0d98eb603151535db5839004125d14a49
Giang Nguyen <tgn1992@hotmail.de>
2018-08-10 13:54:14 +0200

    Send to notebook

--- src/index.js
+++ src/index.js
@@ -1,122 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Chatkit from '@pusher/chatkit';
+import './style/style.scss';
 
-import './style/style.scss'
+import MessageList from './components/MessageList';
 
-function Square(props) {
-      return (
-        <button 
-            className="square" 
-            onClick={() => {props.onClick()
-        }}>
-          {props.value}
-        </button>
-      );
-  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-  
-  class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-    }
-  
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if(calculateWinner(squares) || squares[i]) {
-          return;
-      }
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({squares: squares, xIsNext: !this.state.xIsNext});
-    }
-  
-    renderSquare(i) {
-      return (
-        <Square
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)}
-        />
-      );
-    }
-  
-    render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-          status = 'Winner: ' + winner;
-      } else {
-          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  };
-
-
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+    this.state = {
+      messages: []
     }
   }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+        instanceLocator: 'v1:us1:d34cb8d2-64c7-42ac-8fee-c7a4c48f1566',
+        userId: 'admin', 
+        tokenProvider: new Chatkit.TokenProvider({
+            url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d34cb8d2-64c7-42ac-8fee-c7a4c48f1566/token'
+        })
+    })
+    
+    chatManager.connect()
+    .then(currentUser => {
+        currentUser.subscribeToRoom({
+            roomId: 13513953, 
+            hooks: {
+                onNewMessage: message => {
+                    console.log('message.text: ', message.text);
+                    this.setState({
+                      messages: [...this.state.messages, message]
+                    })
+                }
+            }
+        })
+    })
+}
+
+  
+
+  render() {
+    return (
+      <div className="app">
+        <MessageList messages={this.state.messages}/>
+      </div>
+    )
   }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
