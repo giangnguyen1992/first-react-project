97ac79bb60a757ecca56928235c51d2892087430
Giang <tgn1992@hotmail.de>
2018-08-10 17:06:55 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './style/style.scss';
 
 import MessageList from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
+import RoomList from './components/RoomList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      messages: []
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
     }
+
+    this.sendMessage = this.sendMessage.bind(this)
   }
 
   componentDidMount() {
@@ -26,11 +31,19 @@ class App extends React.Component {
     
     chatManager.connect()
     .then(currentUser => {
-        currentUser.subscribeToRoom({
+        this.currentUser = currentUser
+        this.currentUser.getJoinableRooms()
+        .then(joinableRooms => {
+          this.setState( {
+            joinableRooms,
+            joinedRooms: this.currentUser.rooms
+          })
+        })
+        .catch(err => console.log('error on joinableRooms: ', err))
+        this.currentUser.subscribeToRoom({
             roomId: 13513953, 
             hooks: {
                 onNewMessage: message => {
-                    console.log('message.text: ', message.text);
                     this.setState({
                       messages: [...this.state.messages, message]
                     })
@@ -38,15 +51,22 @@ class App extends React.Component {
             }
         })
     })
+    .catch(err => console.log('error on connecting: ', err))
 }
 
-  
+sendMessage(text) {
+  this.currentUser.sendMessage({
+    text: text,
+    roomId: 13513953
+  })
+}  
 
   render() {
     return (
       <div className="app">
+        <RoomList rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
         <MessageList messages={this.state.messages}/>
-        <SendMessageForm />
+        <SendMessageForm sendMessage={this.sendMessage}/>
       </div>
     )
   }
